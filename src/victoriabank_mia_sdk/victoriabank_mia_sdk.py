e8cd5cf78557c7b70baae2110f039c246c34aa8d
Alexander Minza <alexander.minza@gmail.com>
2025-09-19 17:26:28 +0300

    docstring

--- src/victoriabank_mia_sdk/victoriabank_mia_sdk.py
+++ src/victoriabank_mia_sdk/victoriabank_mia_sdk.py
@@ -10,10 +10,13 @@ logger = logging.getLogger(__name__)
 
 
 # Based on Python SDK for maib MIA API https://github.com/alexminza/maib-mia-sdk-python (https://pypi.org/project/maib-mia-sdk/)
-# IPS Business WebApi https://test-ipspj.victoriabank.md
-# IPS DemoPay WebApi https://test-ipspj-demopay.victoriabank.md/swagger/
 
 class VictoriabankMiaSdk:
+    """
+    * IPS Business WebApi https://test-ipspj.victoriabank.md
+    * IPS DemoPay WebApi https://test-ipspj-demopay.victoriabank.md/swagger/
+    """
+
     # Victoriabank MIA API base urls
     DEFAULT_BASE_URL = 'https://ips-api-pj.vb.md/'
     TEST_BASE_URL = 'https://test-ipspj.victoriabank.md/'
@@ -40,30 +43,6 @@ class VictoriabankMiaSdk:
 
     def __init__(self, base_url: str = DEFAULT_BASE_URL):
         self._base_url = base_url
-
-    def _build_url(self, url: str, entity_id: str = None):
-        """Build the complete URL for the request"""
-
-        if not url.startswith('https://'):
-            url = self._base_url + url
-
-        if entity_id:
-            url = url.format(id=entity_id)
-
-        return url
-
-    def _process_response(self, response: httpx.Response):
-        if response.is_error:
-            logger.error(f'{self.__class__.__qualname__} Error: %d %s', response.status_code, response.text, extra={'method': response.request.method, 'url': response.request.url, 'params': response.request.url.params, 'response_text': response.text, 'status_code': response.status_code})
-            #response.raise_for_status()
-
-        if not response.content:
-            logger.debug(f'{self.__class__.__qualname__} Response: %d', response.status_code, extra={'response_content': response.content})
-            return {}
-
-        response_json: dict = response.json()
-        logger.debug(f'{self.__class__.__qualname__} Response: %d %s %s', response.status_code, response.request.method, response.request.url, extra={'method': response.request.method, 'url': response.request.url, 'params': response.request.url.params, 'response_json': response_json, 'status_code': response.status_code})
-        return response_json
 
     def send_request(self, method: str, url: str, form_data: dict = None, json_data: dict = None, params: dict = None, token: str = None, entity_id: str = None):
         """Send a request and parse the response."""
@@ -118,6 +97,30 @@ class VictoriabankMiaSdk:
             return decoded_payload
         except Exception as ex:
             raise VictoriabankMiaPaymentException(f'Failed to decode and verify payload signature: {ex}') from ex
+
+    def _build_url(self, url: str, entity_id: str = None):
+        """Build the complete URL for the request"""
+
+        if not url.startswith('https://'):
+            url = self._base_url + url
+
+        if entity_id:
+            url = url.format(id=entity_id)
+
+        return url
+
+    def _process_response(self, response: httpx.Response):
+        if response.is_error:
+            logger.error(f'{self.__class__.__qualname__} Error: %d %s', response.status_code, response.text, extra={'method': response.request.method, 'url': response.request.url, 'params': response.request.url.params, 'response_text': response.text, 'status_code': response.status_code})
+            #response.raise_for_status()
+
+        if not response.content:
+            logger.debug(f'{self.__class__.__qualname__} Response: %d', response.status_code, extra={'response_content': response.content})
+            return {}
+
+        response_json: dict = response.json()
+        logger.debug(f'{self.__class__.__qualname__} Response: %d %s %s', response.status_code, response.request.method, response.request.url, extra={'method': response.request.method, 'url': response.request.url, 'params': response.request.url.params, 'response_json': response_json, 'status_code': response.status_code})
+        return response_json
 
 #region Auth
 class BearerAuth(httpx.Auth):
